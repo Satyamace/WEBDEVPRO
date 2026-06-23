7abf9e9dcfa2153eff1e3231f3b5e560429ee826
Satyamace <satyamkumar9916@gmail.com>
2026-06-23 18:22:47 +0530

    asyBehaviour Intro

---
+++ JSFOUNDATION/part8/exp.js
@@ -0,0 +1,16 @@
+
+
+
+function say() {
+    console.log("Hii");
+}
+setTimeout(() => {
+    say()
+}, 5000);
+
+
+console.log("Satyam");
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
